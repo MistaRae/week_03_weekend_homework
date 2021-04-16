4b729bc4341c87663d4bf8e8edf31e8cc0fde4dc
MistaRae <itsamistarae@gmail.com>
2021-04-16 15:33:23 +0100

    game logic working

--- modules/game.py
+++ modules/game.py
@@ -8,5 +8,18 @@ class Game:
         match_drawn = None
         if player_1.gesture == player_2.gesture:
             return f"the winning player is: {match_drawn}"
-        if player_1.gesture == "Rock" and player_2.gesture == "scissors":
+        if player_1.gesture == "Rock" and player_2.gesture == "Scissors":
             return f"{player_1.name} wins by playing {player_1.gesture}!"
+        if player_1.gesture == "Scissors" and player_2.gesture == "Rock":
+            return f"{player_2.name} wins by playing {player_2.gesture}!"
+
+        if player_1.gesture == "Paper" and player_2.gesture == "Rock":
+            return f"{player_1.name} wins by playing {player_1.gesture}!"
+        if player_1.gesture == "Rock" and player_2.gesture == "Paper":
+            return f"{player_2.name} wins by playing {player_2.gesture}!"
+
+        if player_1.gesture == "Scissors" and player_2.gesture == "Paper":
+            return f"{player_1.name} wins by playing {player_1.gesture}!"
+        if player_1.gesture == "Paper" and player_2.gesture == "Scissors":
+            return f"{player_2.name} wins by playing {player_2.gesture}!"
+        
--- tests/test_game.py
+++ tests/test_game.py
@@ -17,4 +17,21 @@ class TestGame(unittest.TestCase):
 
     def test_game_recognises_draw(self):
         self.assertEqual("the winning player is: None", self.game.play(self.mark,self.mark))
+
+    def test_game_rock_beats_scissors(self):
+        self.assertEqual("Mark wins by playing Rock!", self.game.play(self.chris,self.mark))
+
+    def test_game_rock_beats_scissors_reversed(self):
+        self.assertEqual("Mark wins by playing Rock!", self.game.play(self.mark,self.chris))
         
+    def test_game_paper_beats_rock(self):
+        self.assertEqual("Jill wins by playing Paper!", self.game.play(self.jill,self.mark))
+    
+    def test_game_paper_beats_rock_reversed(self):
+        self.assertEqual("Jill wins by playing Paper!", self.game.play(self.mark,self.jill))
+
+    def test_game_scissors_beats_paper(self):
+        self.assertEqual("Chris wins by playing Scissors!", self.game.play(self.chris,self.jill))
+    
+    def test_game_scissors_beats_paper_reversed(self):
+        self.assertEqual("Chris wins by playing Scissors!", self.game.play(self.jill,self.chris))
